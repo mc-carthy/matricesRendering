da40b9c57fc14f6aeb8d79fc3d25b13d2555352e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-07-11 20:37:35 +0100

    Create transformation grid

---
+++ Assets/Scripts/TransformationGrid.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class TransformationGrid : MonoBehaviour {
+
+	public Transform prefab;
+	public int gridResolution = 10;
+
+	Transform[] grid;
+
+	private void Awake () {
+		grid = new Transform[gridResolution * gridResolution * gridResolution];
+		for (int i = 0, z = 0; z < gridResolution; z++) {
+			for (int y = 0; y < gridResolution; y++) {
+				for (int x = 0; x < gridResolution; x++) {
+					grid [i] = CreateGridPoint (x, y, z);
+					grid [i].transform.SetParent (transform);
+				}
+			}
+		}
+	}
+
+	private Transform CreateGridPoint (int x, int y, int z) {
+		Transform point = Instantiate<Transform> (prefab);
+		point.localPosition = GetCoordinates (x, y, z);
+		Color pointColor = new Color (
+			(float)x / gridResolution,
+			(float)y / gridResolution,
+			(float)z / gridResolution
+		);
+		point.GetComponent<MeshRenderer> ().material.color = pointColor;
+		return point;		
+	}
+
+	Vector3 GetCoordinates (int x, int y, int z) {
+		return new Vector3 (
+			x - (gridResolution - 1) * 0.5f,
+			y - (gridResolution - 1) * 0.5f,
+			z - (gridResolution - 1) * 0.5f
+		);
+	}
+}
