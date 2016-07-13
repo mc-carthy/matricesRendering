e8279779e4e8b2208e90e10e395a7177e93455cb
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-07-13 21:22:20 +0100

    Manipulate CameraTransform to show orthographic view

--- Assets/Scripts/CameraTransformation.cs
+++ Assets/Scripts/CameraTransformation.cs
@@ -7,7 +7,7 @@ public class CameraTransformation : Transformation {
 			Matrix4x4 matrix = new Matrix4x4();
 			matrix.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
 			matrix.SetRow(1, new Vector4(0f, 1f, 0f, 0f));
-			matrix.SetRow(2, new Vector4(0f, 0f, 1f, 0f));
+			matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
 			matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
 			return matrix;
 		}
