33f4c8c3af2c583b175ccbcf08858febbdcad07e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-07-13 21:24:32 +0100

    Manipulate CameraTransform to show perspective view

--- Assets/Scripts/CameraTransformation.cs
+++ Assets/Scripts/CameraTransformation.cs
@@ -8,7 +8,7 @@ public class CameraTransformation : Transformation {
 			matrix.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
 			matrix.SetRow(1, new Vector4(0f, 1f, 0f, 0f));
 			matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
-			matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+			matrix.SetRow(3, new Vector4(0f, 0f, 1f, 0f));
 			return matrix;
 		}
 	}
